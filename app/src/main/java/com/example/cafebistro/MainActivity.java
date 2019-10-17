f8874af118a4ae0c887c5ca9403b028d65eca854
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-10-17 18:59:14 +0200

    add EditText in mainactivity

--- app/src/main/java/com/example/cafebistro/MainActivity.java
+++ app/src/main/java/com/example/cafebistro/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.cafebistro;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindRestaurantsButton;
@@ -13,12 +16,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
             }
         });
     }
