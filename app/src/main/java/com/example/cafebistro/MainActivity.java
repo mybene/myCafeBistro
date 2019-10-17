236d686e6b71fcae19171381a7bfdceca60de4d4
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-10-17 18:52:13 +0200

    add a toast on main activity

--- app/src/main/java/com/example/cafebistro/MainActivity.java
+++ app/src/main/java/com/example/cafebistro/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.cafebistro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
